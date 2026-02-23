6bbba828f5615a727638255ed38761df21a8a994
Karthikeyan <karthikeyansivakumar27@gmail.com>
2026-02-23 10:45:13 +0530

    delete

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers, getSystemStats } = require('../controllers/adminController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.get('/users', protect, admin, getUsers);
-router.get('/stats', protect, admin, getSystemStats);
-
-module.exports = router;
--- backend/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateReport, getReports } = require('../controllers/reportController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/').get(protect, getReports).post(protect, generateReport);
-
-module.exports = router;
